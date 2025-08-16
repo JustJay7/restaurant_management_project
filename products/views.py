7adcfe526205e4e3b67cd94e3fd50fd3f0f02386
JustJay7 <jaymalhotra106@gmail.com>
2025-08-16 21:32:37 +0530

    20043

--- products/views.py
+++ products/views.py
@@ -1,18 +1,10 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.response import responsefrom rest_framework import status
 
 from .models import Item
 from .serializers import ItemSerializer
 
-'''
-NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
-'''
-
-# Create your views here.
 class ItemView(APIView):
-
     def get(self, request):
         items = Item.objects.all()
         serializer = ItemSerializer(items, many=True)
@@ -24,3 +16,12 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MenuView(APIView):
+    def get(self, request):
+        data = [
+            {"name": "Margherita Pizza", "description": "Tomato, mozzarella, basil", "price": 9.99},
+            {"name": "Penne Arrabbiata", "description": "Spicy tomato sauce", "price": 11.50},
+            {"name": "Tiramisu", "description": "Espresso-soaked ladyfingers", "price": 6.00},
+        ]
+        return Response(data, status=status.HTTP_200_OK)
