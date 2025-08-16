c1930cd4bbfb0f3d586b22e33e19f503f148e479
JustJay7 <jaymalhotra106@gmail.com>
2025-08-16 21:40:38 +0530

    20042

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Existing Item model
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -8,3 +8,14 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+
+
+# New Menu model
+class Menu(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    items = models.ManyToManyField(Item, related_name="menus")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
